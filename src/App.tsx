b489e228a16bdabeed133725d09f7d761b132be2
Biniam batu <Biniambatu12@gmail.com>
2025-02-26 22:40:24 +0300

    header ad about page

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import About from "./Components/About"
+import Header from "./Components/Header"
+
+
+function App() {
+ 
+  return (
+   <div className="w-full overflow-hidden">
+     <Header/>
+     <About/>
+   </div>
+  )
+}
+
+export default App
